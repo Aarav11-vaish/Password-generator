2f9c1b8ef463de4c693a3e8664619b5435fc9986
Aarav Vaish <aaravvaish2004@gmail.com>
2025-09-02 00:29:19 +0530

    logic changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [length, setLength] = useState(8);
   const [includeNumbers, setIncludeNumbers] = useState(false);
   const [incspecialChars, setIncludeSpecialChars] = useState(false);
+  const [copied, setCopied] = useState("copy");
 
   const generatePassword = useCallback(() => {
     const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -18,6 +19,7 @@ function App() {
     let pass = "";
     if (includeNumbers) {
       s += numbers;
+      s+=numbers;
     }
     if (incspecialChars) {
       s += special;
@@ -29,9 +31,14 @@ function App() {
     setPassword(pass);
   }, [length, includeNumbers, incspecialChars, setPassword]);
   const copytoclipboard = useCallback(()=>{
+setCopied("✅copied!")
+    setTimeout(() => {
+      setCopied("copy")
+    }, 200);
+// setCopied("copy");
     toast.success("Password copied to clipboard")
     navigator.clipboard.writeText(password);
-  }, [password, setPassword, toast])
+  }, [password, setPassword, toast, setCopied, copied])
 
   useEffect(() => {
     generatePassword();
@@ -39,15 +46,15 @@ function App() {
 
   return (
     <div className="bg-slate-900 min-h-screen flex items-center justify-center text-white">
-      <div className="bg-slate-800 p-8 rounded-2xl shadow-lg w-full max-w-md">
+      <div className="bg-slate-800 p-8 rounded-2xl  w-full max-w-md">
         <h1 className="text-2xl font-bold text-center mb-6">
           🔐 Password Generator
         </h1>
 
         {/* Password Display */}
-        <div className="flex items-center gap-2 mb-6">
+        <div className="flex items-center gap-1 mb-6">
           <input
-            className="flex-1 rounded-lg px-3 py-2 border border-slate-600 bg-slate-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="flex-2 rounded-lg px-3 py-2 border border-slate-600 bg-slate-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
             type="text"
             value={password}
             readOnly
@@ -56,7 +63,7 @@ function App() {
             onClick={copytoclipboard}
             className="px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded-lg text-sm font-medium"
           >
-            Copy
+           {copied}
           </button>
         </div>
 
@@ -93,7 +100,7 @@ function App() {
               onChange={(e) => setIncludeSpecialChars(e.target.checked)}
               checked={incspecialChars}
             />
-            <label htmlFor="special" className="text-sm">Include special characters</label>
+            <label  className="text-sm">Include special characters</label>
           </div>
         </div>
       </div>
